2c5d282eab5dcd84ee1fa04d5e20c1e9e26fd090
vandat598 <trandat5998@gmail.com>
2022-06-07 15:38:29 +0700

    updates

--- src/component/Sidebar/MenuItems.js
+++ src/component/Sidebar/MenuItems.js
@@ -2,25 +2,25 @@ export const MenuItems = [
     {
         id: 1,
         title: 'Bản Thân',
-        url: '/',
+        url: '/profile/',
         iconSrc: 'https://cdn.lordicon.com/dxjqoygy.json',
     },
     {
         id: 2,
         title: 'Học Vấn',
-        url: '/education',
+        url: '/profile/education',
         iconSrc: 'https://cdn.lordicon.com/wxnxiano.json',
     },
     {
         id: 3,
         title: 'Mục tiêu',
-        url: '/target',
+        url: '/profile/target',
         iconSrc: 'https://cdn.lordicon.com/hpxruznz.json',
     },
     {
         id: 4,
         title: 'Sở thích',
-        url: '/interests',
+        url: '/profile/interests',
         iconSrc: 'https://cdn.lordicon.com/rjzlnunf.json',
     },
 ];
